835b4de543dae575e0647baac19d155806eaefae
Krishna88894 <krishnaagg0209@fmail.com>
2026-05-18 16:49:50 +0530

    Styling the layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-
+import Sidebar from "./Sidebar.jsx";
+import ChatWindow from "./ChatWindow.jsx";
+import { MyContext } from './Contexts.jsx';
 function App() {
-
-
+  const valueProviders = {};
   return (
     <>
-     
+     <div className="main">
+      <MyContext.Provider values = {valueProviders}></MyContext.Provider>
+      <Sidebar></Sidebar>
+      <ChatWindow> </ChatWindow>
+     </div>
     </>
   )
 }
