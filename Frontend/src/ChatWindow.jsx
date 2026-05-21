00c394ec5685cb729c3a67fa2b64362a1d7e0479
Krishna88894 <krishnaagg0209@fmail.com>
2026-05-21 14:53:52 +0530

    System Design Done

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,28 @@
 import "./ChatWindow.css"
+import Chat from "./Chat.jsx"
 function ChatWindow(){
     return (
-       <div>
-        ChatWindow
-       </div>
+       
+        <div className="chatWindow">
+            <div className="navbar">
+            <span>GPTai
+                <i class="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            <div className="chatInput">
+                <div className="InputBox">
+                    <input placeholder="Ask Anything">
+                    </input>
+                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                GPTai can make mistakes. Check important infomation. 
+                </p>
+            </div>
+        </div>
+       
     )
 }
 export default ChatWindow;
--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,9 +1,19 @@
 import "./Sidebar.css";
 function Sidebar(){
     return (
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+            <button>
+                <img src="src/assets/blacklogo.png" className="logo" alt="GPT Logo" />
+                <span><i class="fa-solid fa-pen-to-square"></i></span>
+            </button>
+            <ul className="history">
+            <li>history</li> 
+            </ul>
+
+            <div className="sign">
+                <p>Made with &hearts;</p>
+            </div>
+        </section>
     )
 }
 export default Sidebar;
